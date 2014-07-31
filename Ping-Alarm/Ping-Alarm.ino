f59acd0cd91de04de0c22fe747d27cef7f36a40c
Barthelemy David <dbarthelemy@imakeit4u.com>
2014-07-31 23:36:34 +0200

    Add Buzzer using Tone Spark Library
    Looks like not all digital pin can be used by Tone.
    e.g. D1 does work but not D6.

--- Ping-Alarm/Ping-Alarm.ino
+++ Ping-Alarm/Ping-Alarm.ino
@@ -5,23 +5,41 @@
 #include "Arduino.h"
 
 const int ledPin = D0;
+const int buzzerPin = D1;
 const int sonarPin = D7;
+const int loopDuration = 500;
+
+const int sonarDistanceMin = 100;
+const int buzzerDuration = 2000;
+const int buzzerFrequence = 2000;
+
+int buzzerCountDown = 0;
 
 Ping sonar(sonarPin);
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
   digitalWrite(ledPin, LOW);
 }
 
 void loop() {
   sonar.fire();
   double distance = sonar.centimeters();
-  if (distance < 50) {
+
+  if ((distance < sonarDistanceMin) && (buzzerCountDown < 0)) {
     digitalWrite(ledPin, HIGH);
-    delay(1000);
+    tone(buzzerPin, buzzerFrequence, 0);
+    buzzerCountDown = buzzerDuration;
   }
-  else {
+  else if (buzzerCountDown == 0) {
     digitalWrite(ledPin, LOW);
+    noTone(buzzerPin);
+    buzzerCountDown = -1;
   }
+  else if (buzzerCountDown > 0) {
+    buzzerCountDown -= loopDuration;
+  }
+  delay(loopDuration);
 }
